13477fa42066cda2c17ebff7005296b7a57fb94b
usamahafiz8 <usamahafiz8@gmail.com>
2024-07-03 01:16:45 +0500

    finalize the button

--- components/Button.js
+++ components/Button.js
@@ -1,17 +1,16 @@
 // components/Button.js
 const Button = ({ text, type = 'primary', ...props }) => {
-    const baseStyles = 'py-2 px-4 rounded-lg text-white';
-    const primaryStyles = 'bg-accent hover:bg-accent-dark';
-    const secondaryStyles = 'bg-transparent border-2 border-accent text-accent hover:bg-accent hover:text-white';
-  
-    const buttonStyles = type === 'primary' ? primaryStyles : secondaryStyles;
-  
-    return (
-      <button className={`${baseStyles} ${buttonStyles}`} {...props}>
-        {text}
-      </button>
-    );
-  };
-  
-  export default Button;
-  
+  const baseStyles = 'py-2 px-4 rounded- text-white';
+  const primaryStyles = 'bg-accent hover:bg-accent-dark';
+  const secondaryStyles = 'bg-transparent border-2 border-accent text-accent hover:bg-accent hover:text-white';
+
+  const buttonStyles = type === 'primary' ? primaryStyles : secondaryStyles;
+
+  return (
+    <button className={`${baseStyles} ${buttonStyles}  rounded-br-xl rounded-tl-xl `} {...props}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
